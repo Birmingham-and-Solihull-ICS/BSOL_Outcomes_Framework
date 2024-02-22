f2c87c13a7a7d43f262ade33795e66a5a9da1d0a
Chris Mainey <c.mainey1@nhs.net>
2024-02-22 18:00:59 +0000

    renamed files for sort order, added assets folder with dbstructure image in

--- R/0000_get_fingertips_metadata.R
+++ R/0000_get_fingertips_metadata.R
@@ -5,7 +5,7 @@ library(jsonlite)
 res <- GET("https://fingertips.phe.org.uk/api/indicator_metadata/by_indicator_id?indicator_ids=253%2C%20255")
             
 
-dt <- fromJSON(rawToChar(res$content))
+dt <- fromJSON(rawToChar(res$content), simplifyDataFrame = TRUE )
 
 
 res$status_code
