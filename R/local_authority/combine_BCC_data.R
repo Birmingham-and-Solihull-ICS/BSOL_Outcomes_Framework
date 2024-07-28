794c7333f60db05f87ff42ded6261bb9dd27dda5
David.Ellis <david.ellis@birmingham.gov.uk>
2024-07-28 16:34:39 +0100

    Updated code to combine BCC OF data

--- R/local_authority/combine_BCC_data.R
+++ R/local_authority/combine_BCC_data.R
@@ -1,40 +1,41 @@
 # Combine BCC data
 
 # Get paths to all BCC data files
-path <- "../data/output/birmingham-source/"
+path <- "../../data/output/birmingham-source/"
 file_paths <- paste(path, list.files(path), sep = "")
 
 # Get ids
 ids <- unlist(lapply(list.files(path), substr, 1, 4))
 
 # Load data
-dfs <- lapply(file_paths, readxl::read_excel)
+dfs <- lapply(file_paths, read.csv)
 
 # OF Aggregation look-up
 OF_aggs <- readxl::read_excel(
-  "../data/OF-Other-Tables.xlsx", sheet = "Aggregation"
+  "../../data/OF-Other-Tables.xlsx", sheet = "Aggregation"
 )
 
 for (i in 1:length(ids)) {
   print(ids[[i]])
   
-  # Join aggregation IDs to Health Check data
-  if (ids[[i]] == "0071") {
+  # Fix demographicID for NDTMS indicators
+  if (ids[[i]] %in% c("0118", "0119")) {
     dfs[[i]] <- dfs[[i]] %>%
-      mutate(index = row_number()) %>%
-      fuzzyjoin::stringdist_join(
-        OF_aggs %>%
-          filter(AggregationType %in% c("PCN","Locality (resident)", "Local Authority")), 
-        by = join_by("AggregationLabel"),
-        method = "jw", #use jw distance metric
-        max_dist=0.1, 
-        distance_col='dist'
-      )%>%
-      group_by(index) %>%
-      slice_min(order_by=dist, n=1) %>%
-      ungroup()
+      mutate(
+        DemographicID = case_when(
+          DemographicID == "Male" ~ 3,
+          DemographicID == "Female" ~ 2,
+          DemographicID == "Persons" ~ 1,     
+          # NAs will be replaced with ID values later
+          DemographicID == "18-29" ~ NA,
+          DemographicID == "30-49" ~ NA,
+          DemographicID == "50+" ~ NA,
+          TRUE ~ NA
+        ),
+        DataQualityID = 1
+      )
   }
-  
+
   # select and reorder columns so they match
   dfs[[i]] <- dfs[[i]] %>%
     select(c("ValueID", "IndicatorID", "InsertDate", "Numerator", "Denominator",       
@@ -44,9 +45,12 @@ for (i in 1:length(ids)) {
 
 # Combine dfs
 OF_values <- bind_rows(dfs)
+# Remove any value IDs
+OF_values$ValueID = ""
 
 # Save output
-writexl::write_xlsx(OF_values, "../data/output/birmingham-OF-values-24-05-24.xlsx")
+write.csv(OF_values, "../../data/output/birmingham-OF-values-28-07-24.csv",
+          row.names = FALSE)
 
 
 
